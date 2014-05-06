bf5db23364aff1fb4670c09e72d16d8a62704199
João Sousa <sousa1989joao@gmail.com>
2014-05-07 00:08:30 +0100

    first commit + example

--- main.cpp
+++ main.cpp
@@ -50,6 +50,18 @@ void InitConsole(void)
 #include "RF24_network/RF24Network.h"
 static unsigned long ulClockMS=0;
 
+// Address of our node
+const uint16_t this_node = 0;
+
+// Address of the other node
+const uint16_t other_node = 1;
+
+// Structure of our payload
+struct payload_t
+{
+  unsigned long ms;
+  unsigned long counter;
+};
 
 
 int main(void)
@@ -79,157 +91,29 @@ int main(void)
 	RF24 radio = RF24();
 	RF24Network network = RF24Network(radio);
 
-	// Radio pipe addresses for the 2 nodes to communicate.
-	const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
-
-	// The debug-friendly names of those roles
-	const char* role_friendly_name[] = { "invalid", "Ping out", "Pong back"};
-
-	// The various roles supported by this sketch
-	typedef enum { role_ping_out = 1, role_pong_back } role_e;
-
-	// The role of the current running sketch
-	role_e role;
-	role = role_pong_back;
-
-	UARTprintf("\n\rRF24/examples/pingpair/\n\r");
-	UARTprintf("ROLE: %s\n\r",role_friendly_name[role]);
-
-	//
-	// Setup and configure rf radio
-	//
 	radio.begin();
-
-	// optionally, increase the delay between retries & # of retries
-	radio.setRetries(15,15);
-
-	// optionally, reduce the payload size.  seems to
-	// improve reliability
-	radio.setPayloadSize(sizeof(int16_t));
-
-	radio.setDataRate(RF24_250KBPS);
-
-	//
-	// Open pipes to other nodes for communication
-	//
-
-	// This simple sketch opens two pipes for these two nodes to communicate
-	// back and forth.
-	// Open 'our' pipe for writing
-	// Open the 'other' pipe for reading, in position #1 (we can have up to 5 pipes open for reading)
-
-	if ( role == role_ping_out )
-	{
-		radio.openWritingPipe(pipes[0]);
-		radio.openReadingPipe(1,pipes[1]);
-	}
-	else
-	{
-		radio.openWritingPipe(pipes[1]);
-		radio.openReadingPipe(1,pipes[0]);
-	}
-
-	//
-	// Start listening
-	//
-
-	radio.startListening();
-
-	//
-	// Dump the configuration of the rf unit for debugging
-	//
-
-	radio.printDetails();
+	network.begin(/*channel*/ 90, /*node address*/ this_node);
 
 
+	UARTprintf("ok...");
+	UARTprintf("RF24Network/examples/helloworld_rx/");
 
 	while (1) {
 
-		//
-		// Ping out role.  Repeatedly send the current time
-		//
 
-		if (role == role_ping_out)
-		{
-			// First, stop listening so we can talk.
-			radio.stopListening();
+		  // Pump the network regularly
+		  network.update();
 
-			// Take the time, and send it.  This will block until complete
-			int16_t time = millis();
-			UARTprintf("Now sending %u...",time);
-			bool ok = radio.write( &time, sizeof(int16_t ));
+		  // If so, grab it and print it out
+		      RF24NetworkHeader header;
+		      payload_t payload;
+		      network.read(header,&payload,sizeof(payload));
+		      UARTprintf("Received packet #");
+		      UARTprintf("%d",payload.counter);
+		      UARTprintf(" at ");
+		      UARTprintf("%d",payload.ms);
 
-			if (ok)
-				UARTprintf("ok...");
-			else
-				UARTprintf("failed.\n\r");
-
-			// Now, continue listening
-			radio.startListening();
-
-			// Wait here until we get a response, or timeout (250ms)
-			unsigned long started_waiting_at = millis();
-			bool timeout = false;
-			while ( ! radio.available() && ! timeout )
-				if (millis() - started_waiting_at > 200 )
-					timeout = true;
-
-			// Describe the results
-			if ( timeout )
-			{
-				UARTprintf("Failed, response timed out.\n\r");
-			}
-			else
-			{
-				// Grab the response, compare, and send to debugging spew
-				int16_t got_time;
-				radio.read( &got_time, sizeof(int16_t));
-
-				// Spew it
-				UARTprintf("Got response %x, round-trip delay: %u\n\r",got_time,millis()-got_time);
-			}
-
-			// Try again 1s later
-			MAP_SysCtlDelay(ulClockMS*1000);
-		}
-
-		//
-		// Pong back role.  Receive each packet, dump it out, and send it back
-		//
-
-		if ( role == role_pong_back )
-		{
-			// if there is data ready
-		if ( radio.available() )
-			{
-				// Dump the payloads until we've gotten everything
-				int16_t	 got_time;
-				bool done = false;
-				while (!done)
-				{
-					// Fetch the payload, and see if this was the last one.
-					done = radio.read( &got_time, sizeof(int16_t) );
-
-					// Spew it
-					UARTprintf("Got payload %x...",got_time);
-
-					// Delay just a little bit to let the other unit
-					// make the transition to receiver
-					MAP_SysCtlDelay(ulClockMS*20);
-				}
-
-				// First, stop listening so we can talk
-				radio.stopListening();
-
-				// Send the final one back.
-				radio.write( &got_time, sizeof(int16_t) );
-				UARTprintf("Sent response.\n\r");
-
-				// Now, resume listening so we catch the next packets.
-				radio.startListening();
-			}
-		}
-
+		      MAP_SysCtlDelay(ulClockMS*1000);
 	}
 
 }
